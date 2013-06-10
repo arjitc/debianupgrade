f9ff475a8f2c4cc98049e80f61c99487bb417ba8
Arjit Chaudhary <arjit.c@gmail.com>
2013-06-10 16:27:31 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export LANGUAGE=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+locale-gen en_US.UTF-8
 rm -rf /etc/apt/sources.list
 wget https://raw.github.com/arjitc/debianupgrade/master/sources.list && mv sources.list /etc/apt/sources.list
 apt-get update
