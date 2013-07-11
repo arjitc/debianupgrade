e04d4e630b02ff1192e477798ea1c453679ab144
Arjit Chaudhary <arjit.c@gmail.com>
2013-07-11 20:37:22 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-export LANGUAGE=en_US.UTF-8
+function script {
 export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-locale-gen en_US.UTF-8
+dpkg-reconfigure locales
 rm -rf /etc/apt/sources.list
 wget https://raw.github.com/arjitc/debianupgrade/master/sources.list && mv sources.list /etc/apt/sources.list
 apt-get update
 apt-get upgrade
 apt-get dist-upgrade
 lsb_release -a
+}
+ 
+time script
